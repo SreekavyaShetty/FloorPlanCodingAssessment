129bea04f63034cd728e8ebe882deda680369ad3
SreekavyaShetty <125827311+SreekavyaShetty@users.noreply.github.com>
2024-01-07 21:15:56 -0700

    Update Blocks.js

--- src/Blocks.js
+++ src/Blocks.js
@@ -1,5 +1,29 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
+// Attaching Tape block at the top of table 
+export const TapeBlock = () => {
+  return (
+    <Typography variant="h6" align="center" style={{ display: 'flex', 
+    alignItems: 'center', 
+    justifyContent: 'center', 
+    marginLeft: '279.5px', 
+    marginTop:'10px',
+    height: '20px',
+    width: `719px`, 
+    fontSize: '10px', 
+    borderLeft: '1px solid black', 
+    color: 'white',
+    borderColor: 'black',
+    borderTop: '1px solid black', 
+    borderRight: '1px solid black', 
+    borderBottom: '0', 
+    fontWeight: 'bold',
+    backgroundColor: '#006699',
+    }}>
+    TAPE-IN DB VIEW
+    </Typography>
+  );
+};
 
 export const IOsBlock = () => {
   return (
